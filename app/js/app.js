ee160c93024005fe0d77669851089254efafea59
lvan226 <lvan226@list.ru>
2020-03-16 19:14:50 +0800

    Main page with errors

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
+	// Слайдер на главной странице
 	$('.main-catalog-slick').slick({
 		slidesToShow: 6,
 		arrows: false,
@@ -8,12 +9,65 @@ document.addEventListener("DOMContentLoaded", function() {
 		dots: false,
 		responsive: [
 			{
+				breakpoint: 1281,
+				settings: {
+					slidesToShow: 5,
+				}
+			},
+			{
 				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 992,
 				settings: {
 					slidesToShow: 3,
 				}
 			},
+			{
+				breakpoint: 992,
+				settings: {
+					variableWidth: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
 		]
 	});
+
+	// Меню в шапке
+	$('.menu-burger').on('click', function() {
+		$('.menu').toggleClass('active');
+		$('.filter-bg').toggleClass('active');
+		$('.logo').toggleClass('active');
+	});
+
+	// Поиск анимация выдвижения
+	// $('.menu__search').on('click', function () {
+	// 	$(this).addClass('active');
+	// });
+
+	$(".menu__search").hover(function() {
+		$(".menu__search").addClass('active');
+	},
+	function() {
+		if (!$(".input-search").val() && !$('.input-search').is(":focus")) {
+			$(".menu__search").removeClass('active');
+		}
+	});
+	$(".menu__search").on('click', function() {
+		$(".menu__search").addClass('active');
+	});
+	$(".input-search").on("input", function() {
+		$(".menu__search").addClass('active');
+	});
+	$('.input-search').focusout(function () {
+		if ($(this).val() === '' ) {
+			$(".menu__search").removeClass('active');
+		}
+	});
+
 
 });
