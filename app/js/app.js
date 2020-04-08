8c464017c24b7aa59f6906d9ed3c1ec6d43e755a
lvan226 <lvan226@list.ru>
2020-04-08 19:56:48 +0800

    Desktop all

--- app/js/app.js
+++ app/js/app.js
@@ -114,7 +114,46 @@ document.addEventListener("DOMContentLoaded", function() {
 			$("#js-range-slider__from").val(data.from);
 			$("#js-range-slider__to").val(data.to);
 		},
-});
+	});
+
+	// ТАБЫ для одного товара
+	$('.productone-info__tab-title > div').on('click', function() {
+		$('.productone-info__tab-title > div').removeClass('active-tab');
+		$(this).addClass('active-tab');
+		var id = $(this).attr('data-tab');
+		$('.productone-info__tab-content > div').removeClass('active-tab');
+		$('.productone-info__tab-content > div[data-tab="' + id + '"]').addClass('active-tab');
+	});
+
+	// Слайдер для карточки товара на странице товара
+	$('.product-one__slider').slick({
+		vertical: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+		verticalSwiping: true,
+		infinite: true,
+		prevArrow: '<button class="slick-prev slick-arrow" type="button" style=""><img src="images/dest/product-one/arrow-slider-top.png" alt=""></button>',
+		nextArrow: '<button class="slick-next slick-arrow" type="button" style=""><img src="images/dest/product-one/arrow-slider-bottom.png" alt=""></button>',
+	});
+
+	// Табы на странице оформление заказа
+	$('.checkout-page__second .checkout-tab__title').on('click', function() {
+		$('.checkout-page__second .checkout-tab__title').removeClass('tab-active');
+		$(this).addClass('tab-active');
+		var id = $(this).attr('data-tab');
+		$('.checkout-page__second .checkout-tab__content').removeClass('tab-active');
+		$('.checkout-page__second .checkout-tab__content[data-tab="' + id + '"]').addClass('tab-active');
+	});
+
+	$('.checkout-page__first .checkout-tab__title').on('click', function() {
+		$('.checkout-page__first .checkout-tab__title').removeClass('tab-active');
+		$(this).addClass('tab-active');
+		var id = $(this).attr('data-tab');
+		$('.checkout-page__first .checkout-tab__content').removeClass('tab-active');
+		$('.checkout-page__first .checkout-tab__content[data-tab="' + id + '"]').addClass('tab-active');
+	});
+
+
 });
 
 // function inputField(input, line) {
